97cc4a04858e303f8b55024aa007666f4b191298
srivathsan99 <100723499+srivathsan99@users.noreply.github.com>
2024-11-29 08:38:23 -0400

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,13 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-// ------------------- 1st error: -------------------
-// test('renders learn react link', () => {
-//   render(<App />);
-//   const linkElement = screen.getByText(/learn react/i);
-// expect(linkElement).toBeInTheDocument();
-// });
-
 // ------------------- 2nd error: -------------------
 // test('renders hello there link', () => {
 //   act(() => {
