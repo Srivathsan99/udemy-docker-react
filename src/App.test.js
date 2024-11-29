c0fc975045f5b5efcc13c6d07623ad2f3bf6b9f3
srivathsan99 <100723499+srivathsan99@users.noreply.github.com>
2024-11-29 08:16:57 -0400

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,34 +1,13 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-import React from 'react';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-// ------------------- 1st error: -------------------
-// test('renders learn react link', () => {
-//   render(<App />);
-//   const linkElement = screen.getByText(/learn react/i);
-// expect(linkElement).toBeInTheDocument();
-// });
-
-// ------------------- 2nd error: -------------------
-// test('renders hello there link', () => {
-//   act(() => {
-//     render(<App />);
-//   });
-//   const linkElement = screen.getByText(/hello there/i); // Updated matcher
-//   expect(linkElement).toBeInTheDocument();
-// });
-
-
-// ------------------- WORKING PART -------------------
-// 'RUN npm install @testing-library/react@latest' was added to the dockerfile
-test('renders Hi there link', () => {
-  render(<App />); // No need for explicit act
-  const linkElement = screen.getByText(/Hi there/i);
+test("renders learn react link", () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
-
-test('renders Hi there link', () => {
-  render(<App />); // No need for explicit act
-  const linkElement = screen.getByText(/Hi there/i);
+test("renders learn react link", () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
