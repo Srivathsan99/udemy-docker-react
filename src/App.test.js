23727bdf451dc8dcd921dbea0bcfd3973d1557d5
srivathsan99 <100723499+srivathsan99@users.noreply.github.com>
2024-11-29 08:47:53 -0400

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,20 +1,20 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-// ------------------- 2nd error: -------------------
-// test('renders hello there link', () => {
-//   act(() => {
-//     render(<App />);
-//   });
-//   const linkElement = screen.getByText(/hello there/i); // Updated matcher
-//   expect(linkElement).toBeInTheDocument();
-// });
-
-
-// ------------------- WORKING PART -------------------
-// 'RUN npm install @testing-library/react@latest' was added to the dockerfile
+// ------------------- 1st error: -------------------
 test('renders Hi there link', () => {
-  render(<App />); // No need for explicit act
-  const linkElement = screen.getByText(/Hi there/i);
+  act(() => {
+    render(<App />);
+  });
+  const linkElement = screen.getByText(/Hi there/i); // Updated matcher
   expect(linkElement).toBeInTheDocument();
 });
+
+// ------------------- WORKING PART -------------------
+  
+////  'RUN npm install @testing-library/react@latest' was added to the dockerfile
+// test('renders Hi there link', () => {
+// render(<App />); // No need for explicit act
+//   const linkElement = screen.getByText(/Hi there/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
